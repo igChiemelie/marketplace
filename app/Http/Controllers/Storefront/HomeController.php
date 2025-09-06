f9c972e7083b49e24211eeedef028c3789d11484
igChiemelie <ezehigc@gmail.com>
2025-09-06 15:28:31 +0100

    Initial commit

--- app/Http/Controllers/Storefront/HomeController.php
+++ app/Http/Controllers/Storefront/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Storefront;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\User;
+use App\Models\Category;
 
 class HomeController extends Controller
 {
@@ -15,12 +16,16 @@ class HomeController extends Controller
             ->take(12)
             ->get();
 
-         $featuredVendors = User::where('role', 'vendor')
+            $featuredVendors = User::where('role', 'vendor')
             ->where('status', 'active') // optional if you have vendor status
-            ->latest()
-            ->take(6)
             ->get();
 
-        return view('home', compact('featured', 'featuredVendors'));
+            $categories = Category::where('status', 'active')->get();
+
+            //show vendor stores
+            // $vendor = User::where('role', 'vendor');
+
+
+        return view('home', compact('featured', 'featuredVendors', 'categories'));
     }
 }
